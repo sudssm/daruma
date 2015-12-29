c484f123295aabed6c144aa8d443857244f88957
Doron Shapiro <shapiro.doron@gmail.com>
2015-12-29 01:35:04 -0500

    Filesystem provider accepts a target location on the FS

--- providers/LocalFilesystemProvider.py
+++ providers/LocalFilesystemProvider.py
@@ -7,18 +7,32 @@ DIRECTORY_MODE = 0o700  # RW only for current user
 
 
 class LocalFilesystemProvider(Provider):
+    def __init__(self, provider_path=""):
+        """
+        Initialize a non-networked provider backed by the local filesystem.
+
+        Args:
+            provider_path: an optional string holding the relative or absolute base path for the backing directory on the filesystem.  Defaults to the current directory.
+        """
+        self.provider_path = provider_path
+        super(LocalFilesystemProvider, self).__init__()
+
+    def __get_translated_filepath(self, relative_filename):
+        return os.path.join(self.provider_path, self.root_dir, relative_filename)
+
     def setup(self):
         try:
-            os.mkdir(self.root_dir, DIRECTORY_MODE)
+            translated_root_dir = self.__get_translated_filepath("")
+            os.makedirs(translated_root_dir, DIRECTORY_MODE)
         except OSError as error:
             if error.errno is not errno.EEXIST:
                 raise
 
     def connect(self):
-        pass
+        pass  # Does nothing - we're always connected to the filesystem!
 
     def get(self, filename):
-        translated_filepath = os.path.join(self.root_dir, filename)
+        translated_filepath = self.__get_translated_filepath(filename)
         try:
             with open(translated_filepath, mode="rb") as target_file:
                 return target_file.read()
@@ -29,12 +43,12 @@ class LocalFilesystemProvider(Provider):
                 raise
 
     def put(self, filename, data):
-        translated_filepath = os.path.join(self.root_dir, filename)
+        translated_filepath = self.__get_translated_filepath(filename)
         with open(translated_filepath, mode="wb") as target_file:
             target_file.write(data)
 
     def delete(self, filename):
-        translated_filepath = os.path.join(self.root_dir, filename)
+        translated_filepath = self.__get_translated_filepath(filename)
         try:
             os.remove(translated_filepath)
         except OSError as error:
