56e9aaa7bf48ec4182aa133e7a1fae88ab153748
michsoch <msoch@seas.upenn.edu>
2016-01-03 22:39:03 -0500

    starting to fill in some of the stub files

--- driver/stubs/FileManager.py
+++ driver/stubs/FileManager.py
@@ -2,57 +2,70 @@
 # this thingy should also eventually handle striping
 import exceptions
 
+
 class FileManager:
 
-  def __init__(self, providers, k_file, master_key):
-    self.providers = providers
-    self.k = k_file
-    self.master_key = master_key
+    def __init__(self, providers, k_file, master_key):
+        self.providers = providers
+        self.k = k_file
+        self.master_key = master_key
 
-    self.get_manifest()
+        self.get_manifest()
 
-  def get_manifest(self):
-    # get shares from providers in parallel
-    # once we have k shares, un-RS the manifest
-    # decrypt the manifest
-    # parse the manifest
+    def get_manifest(self):
+        # get shares from providers in parallel
+        # once we have k shares, un-RS the manifest
+        # decrypt the manifest
+        # parse the manifest
 
-    # if there were providers that didn't have shares of the manifest
-    # (or had invalid shares of the manifest):
-    #   self.refresh()
+        # if there were providers that didn't have shares of the manifest
+        # (or had invalid shares of the manifest):
+        #   self.refresh()
+        pass
 
-  def distribute_manifest(self):
-    # encrypt the manifest
-    # compute RS new shares for the manifest
-    # distribute the shares to all providers
+    def distribute_manifest(self):
+        # encrypt the manifest
+        # compute RS new shares for the manifest
+        # distribute the shares to all providers
 
-  # called when the provider list is inconsistent with the state of the providers
-  def refresh(self):
-    self.distribute_manifest()
-    # for every file, perform a self.get() and self.put()
-    # to fetch it, re-encode it with the new list of providers, and put it
+        # called when the provider list is inconsistent with the state of the providers
+        pass
 
-  def ls (self):
-    # just look at the manifest
-    
-  def get(self, path):
-    # check manifest for path; get random file name
-    # fetch random file name in parallel from providers
-    # once we have enough shares, un-RS it
-    # decrypt
-  
-  def put(self, path, data):
-    # compute RS shares of the file
-    # create a random name for the file
-    # upload RS shares to the random name for all providers
-    # update the manifest with the path,random_name pair
-    # self.distribute_manifest()
-    # if we just replaced a file (instead of putting a fresh one)
-    #   delete the old random_name files from all providers
-  
-  def delete(self, path):
-    # lookup the path in the manifest
-    # update the manifest
-    # self.distribute_manifest()
-    # delete the old random_name files from all providers
-  
+    def refresh(self):
+        self.distribute_manifest()
+        # for every file, perform a self.get() and self.put()
+        # to fetch it, re-encode it with the new list of providers, and put it
+
+    def ls(self):
+        manifest = self.get_manifest()
+        manifest.ls()
+
+    def get_file(self, path, f_key):
+        # check manifest for path; get random file name
+        manifest = self.get_manifest()
+        attributes = manifest.get_line(path)
+
+        # fetch random file name in parallel from providers
+        for provider in self.providers:
+            pass  # get file using attributes[random_name]
+
+        # once we have enough shares, un-RS it
+        # decrypt
+        pass
+
+    def distribite_file(self, path, f_key, data):
+        # compute RS shares of the file
+        # create a random name for the file
+        # upload RS shares to the random name for all providers
+        # update the manifest with the path,random_name pair
+        # self.distribute_manifest()
+        # if we just replaced a file (instead of putting a fresh one)
+        #   delete the old random_name files from all providers
+        pass
+
+    def delete_file(self, path):
+        # lookup the path in the manifest
+        # update the manifest
+        # self.distribute_manifest()
+        # delete the old random_name files from all providers
+        pass
--- driver/stubs/KeyManager.py
+++ driver/stubs/KeyManager.py
@@ -1,33 +1,49 @@
 # This file handles keys using SSSS
+import algos.shamir_secret_sharing
 
-import exceptions
 
 class KeyManager:
-  KEY_FILE_NAME = "the-name-of-our-key-file"
+    KEY_FILE_NAME = "the-name-of-our-key-file"
 
-  def __init__(self, providers, k_key):
-    self.providers = providers
-    self.k = k
+    def __init__(self, providers, k_key):
+        self.providers = providers
+        self.k_key = k_key
 
-  # un SSSS the key from the providers
-  # returns the recovered key from all the shares
-  # throws ProvidersUnconfigured if no provider has a keyshare
-  # throws ProvidersDown if not enough providers have keyshares
-  # probably should throw some sort of connection exception too
-  def recover_key(self):
-    # get all shares from providers in parallel
-    # attempt to recover key
-    # if there were providers that had missing or invalid shares
-    #   self.distribute_key(key)
-    return "1234"
+    # un SSSS the key from the providers
+    # returns the recovered key from all the shares
+    # throws ProvidersUnconfigured if no provider has a keyshare
+    # throws ProvidersDown if not enough providers have keyshares
+    # probably should throw some sort of connection exception too
+    def recover_key(self):
+        # get all shares from providers in parallel
+        shares_map = {}
+        for provider in self.provders:
+            shares_map[provider] = None   # TODO: interface with providers -
+                                          #  None only if no share or provider not reached
+        shares = [value for value in shares_map.values() if value is not None]
+        if (len(shares) < self.key):
+            return (False, None)  # TODO: return error condition to indicate that there are too few shares to reconstruct key
 
-  # distributes the key to all providers
-  # probably throws exception if the provider's put method fails
-  def distribute_key(self, key):
-    # compute new shares using len(providers) and k_key
-    # write shares to providers
-    pass
+        # attempt to recover key
+        (secret_key, share_error) = algos.shamir_secret_sharing.reconstruct(shares)
 
-  # creates a fresh master key
-  def create_key(self):
-    return "1234"
+        success = True
+        # if there were providers that had invalid or missing shares
+        if (len(shares) < len(self.roviders) or share_error):
+            success = False
+
+        return (success, secret_key)
+
+    # distributes a new key to all providers
+    # probably throws exception if the provider's put method fails
+    def distribute_key(self, key):
+        # compute new shares using len(providers) and k_key
+        shares = algos.shamir_secret_sharing.share(key, self.k_key, len(self.providers))
+
+        # write shares to providers
+        for provider in self.providers:
+            pass
+
+    # creates a fresh master key
+    def create_key(self):
+        return "1234"
