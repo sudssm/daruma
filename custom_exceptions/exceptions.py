9cae6201a5c4aa20213928e81656c129889d6c10
michsoch <msoch@seas.upenn.edu>
2016-01-07 22:59:51 -0500

    silly python lint clean up

--- custom_exceptions/exceptions.py
+++ custom_exceptions/exceptions.py
@@ -3,26 +3,38 @@ class DecryptError(Exception):
     """
     Exception for errors in decryption and/or authentication
     """
+
+
 class DecodeError(Exception):
     """
     Exception for errors in decoding an erasure code
     """
+
+
 class SecretReconstructionError(Exception):
     """
     Exception for errors in decoding a secret
     """
 
-# distributor exceptions 
+
+# distributor exceptions
 class ProvidersDown(Exception):
     pass
+
+
 class ProvidersUnconfigured(Exception):
     pass
+
+
 class FileNotFound(Exception):
     pass
+
 
 # manifest exceptions
 class InvalidFormatException(Exception):
     pass
+
+
 class IllegalArgumentException(Exception):
     pass
 
@@ -30,5 +42,7 @@ class IllegalArgumentException(Exception):
 # provider exceptions
 class ProviderFileNotFound(EnvironmentError):
     pass
+
+
 class ProviderConnectionError(EnvironmentError):
     pass
