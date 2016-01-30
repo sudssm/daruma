2555c0a141bf0f315d79c81457f848dcbeca25c7
Doron Shapiro <shapiro.doron@gmail.com>
2016-01-30 13:59:34 -0500

    Log filesystem events using watchdog library

---
+++ gui/file-explorer/FilesystemWatcher.py
@@ -0,0 +1,31 @@
+import logging
+from watchdog.events import FileSystemEventHandler
+
+
+class MyFileSystemEventHandler(FileSystemEventHandler):
+    """Logs all the events captured."""
+
+    def on_moved(self, event):
+        super(MyFileSystemEventHandler, self).on_moved(event)
+
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Moved %s: from %s to %s", what, event.src_path,
+                     event.dest_path)
+
+    def on_created(self, event):
+        super(MyFileSystemEventHandler, self).on_created(event)
+
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Created %s: %s", what, event.src_path)
+
+    def on_deleted(self, event):
+        super(MyFileSystemEventHandler, self).on_deleted(event)
+
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Deleted %s: %s", what, event.src_path)
+
+    def on_modified(self, event):
+        super(MyFileSystemEventHandler, self).on_modified(event)
+
+        what = 'directory' if event.is_directory else 'file'
+        logging.info("Modified %s: %s", what, event.src_path)
