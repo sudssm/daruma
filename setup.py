3b9aab23b16a996294e328caa6445ee0271a9837
Sudarshan Muralidhar <smural@seas.upenn.edu>
2016-02-14 00:25:20 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ setup(
         "PyNaCl==0.3.0",
         "secretsharing==0.2.6",
         "PyECLib==1.1.1",
-        "uuid==1.30"
+        "uuid==1.30",
+        "robustsecretsharing==0.1"
+    ],
+    dependency_links=[
+        "git+ssh://git@github.com/michsoch/robust-secret-sharing.git#egg=robustsecretsharing-0.1"
     ]
+
 )
