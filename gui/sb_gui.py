f3c4b586f99999f70e1e21a0dc1a0bc2684a1aba
Doron Shapiro <shapiro.doron@gmail.com>
2016-02-16 14:05:08 -0500

    Run page client and server together

---
+++ gui/sb_gui.py
@@ -0,0 +1,8 @@
+import threading
+from gui.webview_server.server import start_ui_server
+from gui.osx.app import run_app
+
+if __name__ == "__main__":
+    ui_server_thread = t = threading.Thread(target=start_ui_server, name="ui_server_thread")
+    ui_server_thread.start()
+    run_app()
