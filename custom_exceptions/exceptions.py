30c808dccbb87d83e6bdf3fad335d719f813eceb
Doron Shapiro <shapiro.doron@gmail.com>
2016-02-16 14:37:18 -0500

    Improve gui multiplatform support

--- custom_exceptions/exceptions.py
+++ custom_exceptions/exceptions.py
@@ -94,3 +94,9 @@ class FatalOperationFailure(Exception):
     """
     def __init__(self, failures):
         self.failures = failures
+
+
+class UnsupportedPlatformException(Exception):
+    """
+    Raised when the project is run on an OS it does not support
+    """
--- gui/sb_gui.py
+++ gui/sb_gui.py
@@ -1,8 +1,17 @@
+import sys
 import threading
+from custom_exceptions.exceptions import UnsupportedPlatformException
 from gui.webview_server.server import start_ui_server
-from gui.osx.app import run_app
+from gui.platforms.osx.app import run_app as run_app_osx
+
+
+def start_gui_across_platforms():
+    if sys.platform.startswith('darwin'):
+        run_app_osx()
+    else:
+        raise UnsupportedPlatformException("No support for " + sys.platform)
 
 if __name__ == "__main__":
     ui_server_thread = t = threading.Thread(target=start_ui_server, name="ui_server_thread")
     ui_server_thread.start()
-    run_app()
+    start_gui_across_platforms()
