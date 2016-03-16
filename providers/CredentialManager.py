7d56f4dde1f4ef89769690c0db4876d1e908435d
Ray Lei <raylei@Rays-MBP.fios-router.home>
2016-03-16 02:18:15 -0400

    update credential manager

--- providers/CredentialManager.py
+++ providers/CredentialManager.py
@@ -3,34 +3,52 @@ from appdirs import *
 
 APP_NAME = "Trust No One"
 FILE_NAME = user_config_dir(APP_NAME) + "config.json"
-CREDENTIAL_FILE = open(FILE_NAME, 'rw+')
+
 
 class CredentialManager(object):
 
-	@staticmethod
-	def get_credentials(provider):
-		"""
-        Get user credentials for a provider. Return None if noe presented
+    @staticmethod
+    def get_credentials(provider):
+        """
+        Get user credentials for a provider. Return None if not presented
 
         Args:
             provider: a provider class
         """
-		# load user credentials 
-		try:
-			credentials = json.load(CREDENTIAL_FILE)
-		except ValueError:
-			credentials = {}
 
-		return credentials.get(provider.__class__)
+        credential_file = open(FILE_NAME, 'r')
+        
+        # load user credentials 
+        try:
+            credentials = json.load(credential_file)
+        except ValueError:
+            credentials = {}
+
+        return credentials.get(provider.__name__)
 
 
-	@staticmethod
-	def update_credentials(provider, credentials):
-		try:
-			credentials = json.load(CREDENTIAL_FILE)
-		except ValueError:
-			credentials = {}
+    @staticmethod
+    def update_credentials(provider, credential):
+        """
+        Update user credential for the provider
 
-		credentials[provider.__class__] = credentials
+        Args:
+            provider: a provider class
+            credential: the user credential for that provider
+        """
 
-		json.dump(credentials, CREDENTIAL_FILE)
+        credential_file = open(FILE_NAME, 'r+')
+        try:
+            credentials = json.load(credential_file)
+        except ValueError:
+            credentials = {}
+
+        credentials[provider.__name__] = credential
+
+        # overwrite file
+        credential_file.seek(0)
+        credential_file.write(json.dumps(credentials))
+        credential_file.truncate()
+        credential_file.close()
+
+        
