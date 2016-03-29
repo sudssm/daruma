df66db86231efb2d60a5b882795b64232084cc10
Doron Shapiro <shapiro.doron@gmail.com>
2016-03-29 13:40:07 -0400

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
+import sys
 
-setup(
+# Base settings required for CLI and GUI setup
+base_setup = dict(
     name="trustnoone",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
     setup_requires=[
-        "py2app"
+        "setuptools_git==1.1"
     ],
     install_requires=[
         "PyNaCl==1.0.1",
@@ -27,15 +29,31 @@ setup(
         ":sys_platform == 'darwin'": [
             'pyobjc==3.0.4'
         ]
-    },
+    }
+)
+
+# General settings needed for app builds
+build_setup = dict(
     app=["gui/sb_gui.py"],
     data_files=["gui/webview_server/templates", 'gui/webview_server/static'],
-    options={
-        "py2app": {
-            "packages": ['jinja2', 'flask'],
-            'plist': {
-                'LSUIElement': True,
-            }
+    options={}
+)
+
+# Platform-specific build setup
+if sys.platform == "darwin":
+    build_setup["options"]["py2app"] = {
+        "packages": ['jinja2', 'flask'],
+        'plist': {
+            'LSUIElement': True,
         }
     }
+
+    base_setup["setup_requires"].extend([
+        "py2app==0.10"
+    ])
+
+# Combine it all
+base_setup.update(build_setup)
+setup(
+    **base_setup
 )
