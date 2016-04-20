a6e742a7ddde9432be69bc86005dc593e922ac5c
Doron Shapiro <shapiro.doron@gmail.com>
2016-04-20 16:58:45 -0400

    Update build script with new branding

--- build.py
+++ build.py
@@ -9,8 +9,8 @@ def build_app():
     def build_app_osx():
         subprocess.call(["python", "setup.py", "py2app"])
         subprocess.call(["xcodebuild", "-target", "FileSyncClient"], cwd="gui/file_sync_status/osx")
-        subprocess.call(["mkdir", "dist/trustnoone.app/Contents/PlugIns"])
-        subprocess.call(["cp", "-r", "gui/file_sync_status/osx/build/Release/FileSyncClient.appex", "dist/trustnoone.app/Contents/PlugIns/"])
+        subprocess.call(["mkdir", "dist/daruma.app/Contents/PlugIns"])
+        subprocess.call(["cp", "-r", "gui/file_sync_status/osx/build/Release/FileSyncClient.appex", "dist/daruma.app/Contents/PlugIns/"])
     if sys.platform.startswith('darwin'):
         build_app_osx()
     else:
