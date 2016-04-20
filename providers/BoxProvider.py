655a6d242334853fe1ae4b7a0ebba6ec0b0410ea
michsoch <msoch@seas.upenn.edu>
2016-04-19 20:47:16 -0400

    update to use decorator

--- providers/BoxProvider.py
+++ providers/BoxProvider.py
@@ -8,6 +8,24 @@ from StringIO import StringIO
 from requests.exceptions import ReadTimeout
 import json
 
+def try_with_refresh(func):
+    """
+    Runs func. If func raises an AuthFailure, refreshes tokens and tries again.
+    Updates self.auth_token and self.refresh_token
+    Write updated credentials to the internal credential_manager after running func
+    """
+    def refresh_wrapper(*args, **kwargs):
+        self = args[0]     
+        try:
+            return func(*args, **kwargs)
+        except exceptions.AuthFailure:
+            with self.exception_handler():
+                # self.auth_token and self.refresh_token are updated in store_tokens_callback
+                self.client.auth.refresh(self.access_token)
+            result = func(*args, **kwargs)
+            self._persist_tokens()
+            return result
+    return refresh_wrapper
 
 class BoxProvider(OAuthProvider):
     BOX_ROOT_ID = '0'  # The root of the Box system (per Box docs)
@@ -50,23 +68,6 @@ class BoxProvider(OAuthProvider):
         except Exception:
             raise exceptions.ProviderOperationFailure(self)
 
-    def _try_with_refresh(self, func):
-        """
-        Runs func. If func raises an AuthFailure, refreshes tokens and tries again.
-        Updates self.auth_token and self.refresh_token
-        Write updated credentials to the internal credential_manager after running func
-        """
-        try:
-            with self.exception_handler():
-                return func()
-        except exceptions.AuthFailure:
-            with self.exception_handler():
-                # self.auth_token and self.refresh_token are updated in store_tokens_callback
-                self.client.auth.refresh(self.access_token)
-                result = func()
-                self._persist_tokens()
-                return result
-
     def start_connection(self):
         self.oauth = OAuth2(client_id=self.app_credentials["client_id"], client_secret=self.app_credentials["client_secret"])
         with self.exception_handler():
@@ -106,16 +107,14 @@ class BoxProvider(OAuthProvider):
         user_credentials = {"access_token": self.access_token, "refresh_token": self.refresh_token}
         self.credential_manager.set_user_credentials(self.__class__, self.uid, user_credentials)
 
-    def _connect(self, user_credentials):
-        def store_tokens_callback(access_token, refresh_token):
-            self.access_token = access_token
-            self.refresh_token = refresh_token
+    @try_with_refresh
+    def load_email(self):
+        with self.exception_handler():
+            self._email = self.client.user(user_id='me').get()['login']
 
-        def load_email():
-            with self.exception_handler():
-                self._email = self.client.user(user_id='me').get()['login']
-
-        def make_app_folder():
+    @try_with_refresh
+    def make_app_folder(self):
+        with self.exception_handler():
             box_root_folder = self.client.folder(self.BOX_ROOT_ID)
 
             try:  # make an app-specific folder if one does not already exist
@@ -125,7 +124,9 @@ class BoxProvider(OAuthProvider):
 
             self.app_folder = self.client.folder(folder_id)
 
-        def prime_cache():
+    @try_with_refresh
+    def prime_cache(self):
+        with self.exception_handler():
             # get all items
             files = []
             offset = 0
@@ -133,6 +134,11 @@ class BoxProvider(OAuthProvider):
                 files += self.app_folder.get_items(self.MAX_BOX_LIMIT, offset=offset)
                 offset += self.MAX_BOX_LIMIT
             self.id_cache = {user_file.name: user_file.object_id for user_file in files}
+
+    def _connect(self, user_credentials):
+        def store_tokens_callback(access_token, refresh_token):
+            self.access_token = access_token
+            self.refresh_token = refresh_token
 
         # if this came from cache, it is a json string that needs to be converted
         if type(user_credentials) in [unicode, str]:
@@ -148,9 +154,9 @@ class BoxProvider(OAuthProvider):
 
         self.client = Client(oauth)
 
-        self._try_with_refresh(load_email)
-        self._try_with_refresh(make_app_folder)
-        self._try_with_refresh(prime_cache)
+        self.load_email()
+        self.make_app_folder()
+        self.prime_cache()
 
         self._persist_tokens()
 
@@ -158,17 +164,17 @@ class BoxProvider(OAuthProvider):
     def uid(self):
         return self._email
 
+    @try_with_refresh
     def get(self, filename):
-        def try_get():
+        with self.exception_handler():
             box_file = self.client.file(self.id_cache[filename])
             return box_file.content()
 
-        return self._try_with_refresh(try_get)
-
+    @try_with_refresh
     def put(self, filename, data):
         data_stream = StringIO(data)
 
-        def try_put():
+        with self.exception_handler():
             if filename in self.id_cache:
                 existing_file = self.client.file(self.id_cache[filename])
                 existing_file.update_contents_with_stream(data_stream)
@@ -176,18 +182,16 @@ class BoxProvider(OAuthProvider):
                 new_file = self.app_folder.upload_stream(data_stream, filename)
                 self.id_cache[filename] = new_file.object_id
 
-        self._try_with_refresh(try_put)
-
+    @try_with_refresh
     def delete(self, filename):
-        def try_delete():
+        with self.exception_handler():
             box_file = self.client.file(self.id_cache[filename])
             box_file.delete()
             self.id_cache.pop(filename, None)
 
-        self._try_with_refresh(try_delete)
-
+    @try_with_refresh
     def wipe(self):
-        def try_wipe():
+        with self.exception_handler():
             try:
                 for _, file_id in self.id_cache.items():
                     box_file = self.client.file(file_id)
@@ -197,4 +201,3 @@ class BoxProvider(OAuthProvider):
             finally:
                 self.id_cache = {}
 
-        self._try_with_refresh(try_wipe)
