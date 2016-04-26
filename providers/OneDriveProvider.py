f88581bf33952e2908d2003a6209530b3420d310
Ray Lei <raylei@Rays-MBP.fios-router.home>
2016-04-25 22:48:05 -0400

    test onedrive

--- providers/OneDriveProvider.py
+++ providers/OneDriveProvider.py
@@ -7,6 +7,7 @@ from contextlib import contextmanager
 from providers.OAuthProvider import OAuthProvider
 from custom_exceptions import exceptions
 from tools.utils import parse_url
+import io
 
 
 class OneDriveProvider(OAuthProvider):
@@ -94,8 +95,11 @@ class OneDriveProvider(OAuthProvider):
     def uid(self):
         return self.email
 
+    def _get_internal_name(self, filename):
+        return self.ROOT_DIR + "/" + filename
+
     def _get_item(self, filename):
-        return self.client.item(path=self.ROOT_DIR + "/" + filename)
+        return self.client.item(path=self._get_internal_name(filename))
 
     def get(self, filename):
         with self.exception_handler():
@@ -103,17 +107,52 @@ class OneDriveProvider(OAuthProvider):
             self.client.auth_provider.authenticate_request(request)
             response = requests.get(request.request_url, headers=request._headers)
 
-            # create a response. This will throw appropriate errors as needed
-            onedrivesdk.http_response.HttpResponse(response.status_code, response.headers, response.text)
+            if response.status_code != 200:
+                # create a response. This will throw appropriate errors as needed
+                custom_response = onedrivesdk.http_response.HttpResponse(response.status_code, response.headers, response.text)
+                # in case it didn't throw
+                assert custom_response.status == 200
 
             return ''.join(response.iter_content())
 
     def put(self, filename, data):
         with self.exception_handler():
-            request = self._get_item(filename).content.request()
-            request.append_option(onedrivesdk.options.HeaderOption("Content-Type", "application/octet-stream"))
-            self.client.auth_provider.authenticate_request(request)
-            self.client.http_provider.send("PUT", request._headers, request.request_url, data=data)
+            max_chunk_size = 62914560  # 60 mb
+            size = len(data)
+            if size < max_chunk_size:
+                request = self._get_item(filename).content.request()
+                request.append_option(onedrivesdk.options.HeaderOption("Content-Type", "application/octet-stream"))
+                self.client.auth_provider.authenticate_request(request)
+                response = self.client.http_provider.send("PUT", request._headers, request.request_url, data=data)
+
+                assert response.status >= 200 and response.status < 300
+            else:
+                # create a blank file
+                self.put(filename, "")
+
+                f = io.BytesIO(data)
+
+                item = self._get_item(filename)
+                new_item = onedrivesdk.Item()
+                new_item.name = filename
+                response = item.create_session(new_item).post()
+
+                upload_url = response.upload_url
+
+                request = item.content.request()
+                request.append_option(onedrivesdk.options.HeaderOption("Content-Type", "application/octet-stream"))
+                self.client.auth_provider.authenticate_request(request)
+
+                offset = 0
+                while offset < size:
+                    chunk = f.read(max_chunk_size)
+                    chunklen = len(chunk)
+                    request.append_option(onedrivesdk.options.HeaderOption(
+                        "Content-Range", "bytes %s-%s/%s" % (offset, offset+chunklen-1, size)))
+                    response = self.client.http_provider.send("PUT", request._headers, upload_url, data=chunk)
+
+                    assert response.status >= 200 and response.status < 300
+                    offset += chunklen
 
     def get_capacity(self):
         # TODO
